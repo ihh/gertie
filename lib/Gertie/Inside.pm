a123fb40594ff127da8462001670d8e0abf5f8cc
Ian Holmes <github@yam.biowiki.org>
2012-02-09 15:52:57 -0800

    Access to p,q matrices is now via accessors for many Inside methods

--- lib/Gertie/Inside.pm
+++ lib/Gertie/Inside.pm
@@ -36,6 +36,24 @@ sub new_Inside {
     return $self;
 }
 
+# accessors
+sub get_p {
+    my ($self, $i, $j, $sym) = @_;
+    my $val = $self->p->[$j]->[$i]->{$sym};
+    return defined($val) ? $val : 0;
+}
+
+sub get_q {
+    my ($self, $i, $sym) = @_;
+    my $val = $self->q->[$i]->{$sym};
+    return defined($val) ? $val : 0;
+}
+
+sub len {
+    my ($self) = @_;
+    return @{$self->tokseq} + 0;
+}
+
 # subroutine to extend Inside matrix
 sub push_tok {
     my ($self, @new_tok) = @_;
@@ -169,18 +187,12 @@ sub pop_sym {
 
 sub final_p {
     my ($self) = @_;
-    my $p = $self->p;
-    my $len = $#$p;
-    my $cell = $p->[$len]->[0];
-    my $start = $self->gertie->start_id;
-    return defined($cell->{$start}) ? $cell->{$start} : 0;
+    return $self->get_p (0, $self->len, $self->gertie->start_id);
 }
 
 sub final_q {
     my ($self) = @_;
-    my $cell = $self->q->[0];
-    my $start = $self->gertie->start_id;
-    return defined($cell->{$start}) ? $cell->{$start} : 0;
+    return $self->get_q (0, $self->gertie->start_id);
 }
 
 sub final_total {
@@ -200,18 +212,21 @@ sub to_string {
     my $q = $self->q;
     my $len = $#$p;
     my @out;
+    my @sym = @{$self->gertie->sym_name};
     for (my $i = $len; $i >= 0; --$i) {
 
 	push @out, "Prefix $i..:";
-	for my $sym (sort {$a<=>$b} keys %{$q->[$i]}) {
-	    push @out, " ", $gertie->sym_name->[$sym], "=>", $q->[$i]->{$sym};
+	for my $sym_id (0..$#sym) {
+	    my $qval = $self->get_q ($i, $sym_id);
+	    push @out, " ", $sym[$sym_id], "=>", $qval if $qval > 0;
 	}
 	push @out, "\n";
 
 	for (my $j = $i; $j <= $len; ++$j) {
 	    push @out, "Inside ($i,$j):";
-	    for my $sym (sort {$a<=>$b} keys %{$p->[$j]->[$i]}) {
-		push @out, " ", $gertie->sym_name->[$sym], "=>", $p->[$j]->[$i]->{$sym};
+	    for my $sym_id (0..$#sym) {
+		my $pval = $self->get_p ($i, $j, $sym_id);
+		push @out, " ", $sym[$sym_id], "=>", $pval if $pval > 0;
 	    }
 	    push @out, "\n";
 	}
@@ -222,9 +237,7 @@ sub to_string {
 sub traceback {
     my ($self) = @_;
     my $gertie = $self->gertie;
-    my $p = $self->p;
-    my $q = $self->q;
-    my $len = $#$p;
+    my $len = $self->len;
     my $q_prob = $self->final_q;
     my $p_prob = $self->final_p;
     my $is_complete = Gertie::sample ([defined($q_prob) ? $q_prob : 0,
@@ -241,19 +254,17 @@ sub traceback_p {
     my ($self, $i, $j, $lhs) = @_;
     my $gertie = $self->gertie;
     return [$gertie->sym_name->[$lhs]] if $gertie->is_term->{$lhs};
-    my $p = $self->p;
-    my $q = $self->q;
     my (@rhs_k, @prob);
     my $rule_by_rhs1 = $gertie->rule_by_lhs_rhs1->{$lhs};
     confess "Traceback error: i=$i, j=$j, lhs=", $gertie->sym_name->[$lhs] unless defined $rule_by_rhs1;
     for (my $k = $i; $k <= $j; ++$k) {
 	for my $rhs1 (keys %$rule_by_rhs1) {
-	    if (defined $p->[$k]->[$i]->{$rhs1}) {
+	    if (my $rhs1_prob = $self->get_p($i,$k,$rhs1)) {
 		for my $rule (@{$rule_by_rhs1->{$rhs1}}) {
 		    my ($rhs2, $rule_prob, $rule_index) = @$rule;
-		    if (defined $p->[$j]->[$k]->{$rhs2}) {
+		    if (my $rhs2_prob = $self->get_p($j,$k,$rhs2)) {
 			push @rhs_k, [$rhs1, $rhs2, $k];
-			push @prob, $p->[$k]->[$i]->{$rhs1} * $p->[$j]->[$k]->{$rhs2} * $rule_prob;
+			push @prob, $rhs1_prob * $rhs2_prob * $rule_prob;
 		    }
 		}
 	    }
@@ -269,30 +280,28 @@ sub traceback_p {
 sub traceback_q {
     my ($self, $i, $lhs) = @_;
     my $gertie = $self->gertie;
-    my $p = $self->p;
-    my $q = $self->q;
-    my $len = $#$p;
+    my $len = $self->len;
     return [$gertie->sym_name->[$lhs]] if $gertie->is_term->{$lhs} && $i == $len;
     my (@rhs_k, @prob);
     my $rule_by_rhs1 = $gertie->rule_by_lhs_rhs1->{$lhs};
     confess "Traceback error: i=$i, lhs=", $gertie->sym_name->[$lhs] unless defined $rule_by_rhs1;
     while (my ($rhs1, $rule_list) = each %$rule_by_rhs1) {
 	for (my $k = $i; $k <= $len; ++$k) {
-	    if (defined $p->[$k]->[$i]->{$rhs1}) {
+	    if (my $rhs1_prob = $self->get_p($i,$k,$rhs1)) {
 		for my $rule (@$rule_list) {
 		    my ($rhs2, $rule_prob, $rule_index) = @$rule;
-		    if (defined $q->[$k]->{$rhs2}) {
+		    if (my $rhs2_prob = $self->get_q($k,$rhs2)) {
 			push @rhs_k, [$rhs1, $rhs2, $k];
-			push @prob, $p->[$k]->[$i]->{$rhs1} * $q->[$k]->{$rhs2} * $rule_prob;
+			push @prob, $rhs1_prob * $rhs2_prob * $rule_prob;
 		    }
 		}
 	    }
 	}
-	if (defined $q->[$i]->{$rhs1}) {
+	if (my $rhs1_prob = $self->get_q($i,$rhs1)) {
 	    for my $rule (@$rule_list) {
 		my ($rhs2, $rule_prob, $rule_index) = @$rule;
 		push @rhs_k, [$rhs1, $rhs2, $len + 1];
-		push @prob, $q->[$i]->{$rhs1} * $rule_prob;
+		push @prob, $rhs1_prob * $rule_prob;
 	    }
 	}
     }
