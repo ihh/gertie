ce8d0bd5b34be6ab9d317c5b99ce1e4af3ef6c22
Ian Holmes <ihh@berkeley.edu>
2012-02-26 16:26:37 -0800

    More DAMSL-related scripts

---
+++ damsl/map-transcript.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl -w
+my (%seen,%gertie);
+open F,"<damsl-mapping.txt";while(<F>){my ($d,$g)=split;$gertie{$d}=$g}close F;
+my $started;
+for my $file (@ARGV) {
+    open F, "<$file";
+while(<F>){print unless $started;++$started if/^===/;next unless $started;if(/^(\S+)\s+([A-Z])\.\d+ utt\d+: (.*?)\s*\/\s*$/){my($d,$s,$t)=($1,$2,$3);unless(defined$gertie{$d}){for$k(sort{length($b)<=>length($a)}keys%gertie){if(substr($d,0,length($k))eq$k){$gertie{$d}=$gertie{$k};warn"Adding $d -> $k\n";last}}}if(defined$gertie{$d}){print"$gertie{$d}\@$s $t\n"}else{warn"Skipped: $d\n"unless$seen{$d}++}}}
+    close F;
+}
+
