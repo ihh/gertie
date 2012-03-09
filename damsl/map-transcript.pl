2bb5eb945b02a6a94748932ab968e57abc065942
Ian Holmes <ihh@berkeley.edu>
2012-03-08 23:08:46 -0800

    fixed some bugs in the DAMSL scripts for converting the Switchboard corpus

--- damsl/map-transcript.pl
+++ damsl/map-transcript.pl
@@ -4,7 +4,7 @@ open F,"<damsl-mapping.txt";while(<F>){my ($d,$g)=split;$gertie{$d}=$g}close F;
 my $started;
 for my $file (@ARGV) {
     open F, "<$file";
-while(<F>){print unless $started;++$started if/^===/;next unless $started;if(/^(\S+)\s+([A-Z])\.\d+ utt\d+: (.*?)\s*\/\s*$/){my($d,$s,$t)=($1,$2,$3);unless(defined$gertie{$d}){for$k(sort{length($b)<=>length($a)}keys%gertie){if(substr($d,0,length($k))eq$k){$gertie{$d}=$gertie{$k};warn"Adding $d -> $k\n";last}}}if(defined$gertie{$d}){print"$gertie{$d}\@$s $t\n"}else{warn"Skipped: $d\n"unless$seen{$d}++}}}
+while(<F>){if(/^===/){++$started;next}next unless $started;if(/^(\S+)\s+([A-Z])\.\d+ utt\d+: (.*?)\s*\/\s*$/){my($d,$s,$t)=($1,$2,$3);unless(defined$gertie{$d}){for$k(sort{length($b)<=>length($a)}keys%gertie){if(substr($d,0,length($k))eq$k){$gertie{$d}=$gertie{$k};warn"Adding $d -> $k\n";last}}}if(defined$gertie{$d}){print"$gertie{$d}\@$s $t\n"}else{warn"Skipped: $d\n"unless$seen{$d}++}}}
     close F;
 }
 
